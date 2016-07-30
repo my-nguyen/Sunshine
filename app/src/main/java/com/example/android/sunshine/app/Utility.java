3bd68b7bcba45b8b2a9ce92ceb07e7deaf1bb72b
iceguyvn <nguyen_my@yahoo.com>
2016-07-29 23:20:36 -0700

    finished Notifications (video 16/23)

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -33,9 +33,13 @@ public class Utility {
             .equals(context.getString(R.string.pref_units_value_metric));
    }
 
-   public static String formatTemperature(Context context, double temperature, boolean isMetric) {
-      double temp = isMetric ? temperature : 9*temperature/5+32;
-      return context.getString(R.string.format_temperature, temp);
+   public static String formatTemperature(Context context, double temperature) {
+      // Data stored in Celsius by default.  If user prefers to see in Fahrenheit, convert the values here.
+      String suffix = "\u00B0";
+      if (!isMetric(context)) {
+         temperature = (temperature * 1.8) + 32;
+      }
+      return String.format(context.getString(R.string.format_temperature), temperature);
    }
 
    public static String formatDate(long dateInMillis) {
